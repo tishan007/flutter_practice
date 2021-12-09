5efd6afbfacc5bd399c45b57963e82810f1a0df4
Mimu <mimu.saha@sebpo.com>
2021-12-09 17:58:59 +0600

    add form textfield with prefix and suffix icon design

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,15 +13,90 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        resizeToAvoidBottomPadding: false,
         appBar: AppBar(
           centerTitle: true,
           title: Text("Flutter"),
           backgroundColor: Colors.redAccent,
         ),
-        body: Center(child: Text("Happy Coding!")),
+        body: Center(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: screenSize.width - 80,
+              height: screenSize.height * 0.65,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Mobile Number",style: TextStyle(color: Colors.black54,fontWeight: FontWeight.bold, fontSize: 18),),
+                  SizedBox(height: 5,),
+                  TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: new BorderSide(color: Colors.black26)
+                      ),
+                      filled: true,
+                      fillColor: Colors.white54,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Text("Mobile Operator",style: TextStyle(color: Colors.black54,fontWeight: FontWeight.bold, fontSize: 18),),
+                  SizedBox(height: 5,),
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.network_wifi,color: Colors.grey,),
+                      suffixIcon: Icon(Icons.arrow_downward,color: Colors.grey,),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: new BorderSide(color: Colors.black26)
+                      ),
+                      filled: true,
+                      hintStyle: TextStyle(color: Colors.black),
+                      fillColor: Colors.white54,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Text("Mobile Operator Type",style: TextStyle(color: Colors.black54,fontWeight: FontWeight.bold, fontSize: 18),),
+                  SizedBox(height: 5,),
+                  TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: new BorderSide(color: Colors.black26)
+                      ),
+                      filled: true,
+                      hintStyle: TextStyle(color: Colors.black),
+                      fillColor: Colors.white54,
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Align(
+                      alignment: Alignment.center,
+                      child: RaisedButton(
+                        onPressed: () {
+
+                        },
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+                        padding: const EdgeInsets.all(0.0),
+                        child: Container(
+                          height: 50,
+                          alignment: Alignment.center,
+                          color: Colors.deepOrange,
+                          child: const Text(
+                            'SUBMIT', style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      )
+                  )
+                ],
+              ),
+            ),
+        ),
       ),
     );
   }
