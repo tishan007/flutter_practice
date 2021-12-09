eb017c55f8f9803bcd775f87f6870a33a2101dbd
Mimu <mimu.saha@sebpo.com>
2021-12-09 16:36:23 +0600

    initial commit of full repository

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Flutter"),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Center(child: Text("Happy Coding")),
+      ),
+    );
+  }
+}
+
